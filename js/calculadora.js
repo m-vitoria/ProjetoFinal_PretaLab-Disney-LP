5475e2be4f072401c190828f3d662f1c13de4915
m-vitoria <vitoriadassilva45@gmail.com>
2023-03-11 17:23:30 -0300

    projeto one

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,12 +6,13 @@ function somar() {
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
   
+  const soma = numero1 + numero2;
+  console.log(soma);
   
   
   
   
-  
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = soma;
 }
 
 function subtrair() {
@@ -23,11 +24,12 @@ function subtrair() {
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
   
   
+  const subtracao = numero1 - numero2;
+  console.log(subtracao);
   
   
   
-  
-  document.getElementById("resultadoSub").innerHTML = 9;
+  document.getElementById("resultadoSub").innerHTML = subtracao;
 }
 
 function multiplicar() {
@@ -39,9 +41,10 @@ function multiplicar() {
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
   
   
+  const multi = numero1 * numero2;
+  console.log(multi);
   
-  
-  document.getElementById("resultadoMult").innerHTML = 5;
+  document.getElementById("resultadoMult").innerHTML = multi;
 }
 
 function dividir() {
@@ -52,9 +55,9 @@ function dividir() {
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
   
+  const divisao = numero1 / numero2;
+  console.log(divisao)
   
   
-  
-  
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = divisao;
 }
